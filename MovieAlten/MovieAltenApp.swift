58a6ae9471148dfe42726b96f637791dd4c00b86
Sergio <sergio.munoz@ecommfarm.com>
2021-12-19 16:42:12 +0100

    Initial Commit

---
+++ MovieAlten/MovieAltenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAltenApp.swift
+//  MovieAlten
+//
+//  Created by Sergio Muñoz on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieAltenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
